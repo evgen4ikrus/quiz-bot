146baac727249ad6cd63e8aa2a22c4d2c504bfa2
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2022-10-25 10:31:08 +0300

    add menu keyboard

--- tg_bot.py
+++ tg_bot.py
@@ -1,18 +1,27 @@
 import logging
 
-import telegram
 from environs import Env
-from telegram.ext import (CallbackContext, CommandHandler, Filters,
-                          MessageHandler, Updater)
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
 logger = logging.getLogger(__name__)
 
 
-def start(update: telegram.Update, context: CallbackContext):
-    update.message.reply_text('Привет!')
+def start(update, _):
+    menu_keyboard = [
+        [
+            InlineKeyboardButton('Новый вопрос', callback_data='1'),
+            InlineKeyboardButton('Сдаться', callback_data='2'),
+        ],
+        [
+            InlineKeyboardButton('Мой счет', callback_data='3')
+        ],
+    ]
+    reply_markup = InlineKeyboardMarkup(menu_keyboard)
+    update.message.reply_text('Привет! Я бот для викторин.', reply_markup=reply_markup)
 
 
-def echo(update: telegram.Update, context: CallbackContext):
+def echo(update, _):
     update.message.reply_text(update.message.text)
 
 
@@ -21,7 +30,6 @@ def main():
     env = Env()
     env.read_env()
     tg_token = env('TG_TOKEN')
-
     updater = Updater(tg_token)
     dispatcher = updater.dispatcher
 
