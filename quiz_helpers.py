b8c93af99bafab2ce0fb3027126077edb13950f1
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2022-11-02 15:19:21 +0300

    check user answer

--- quiz_helpers.py
+++ quiz_helpers.py
@@ -5,6 +5,12 @@ import random
 def get_random_question():
     with open('quiz_bank.json', 'r', encoding='UTF-8') as file:
         quiz_bank = json.load(file)
-    question, _ = random.choice(list(quiz_bank.items())) 
+    question, _ = random.choice(list(quiz_bank.items()))
     return question
-        
+
+
+def get_answer(question):
+    with open('quiz_bank.json', 'r', encoding='UTF-8') as file:
+        quiz_bank = json.load(file)
+    answer = quiz_bank.get(question)
+    return answer
--- tg_bot.py
+++ tg_bot.py
@@ -3,7 +3,7 @@ import logging
 import telegram
 from environs import Env
 from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
-from quiz_helpers import get_random_question
+from quiz_helpers import get_answer, get_random_question
 from redis_tools import auth_redis
 from functools import partial
 
@@ -12,19 +12,33 @@ logger = logging.getLogger('tg_bot')
 
 
 def start(bot, update):
-    custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счет']]
-    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    menu_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счет']]
+    reply_markup = telegram.ReplyKeyboardMarkup(menu_keyboard)
     chat_id = str(update.effective_user.id)
     bot.send_message(chat_id=chat_id, text='Привет, я бот для викторин!', reply_markup=reply_markup)
 
 
 def echo(bot, update, redis_db):
+    menu_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счет']]
+    reply_markup = telegram.ReplyKeyboardMarkup(menu_keyboard)
     message_text = update.message.text
     chat_id = str(update.effective_user.id)
     if message_text == 'Новый вопрос':
         question = get_random_question()
-        update.message.reply_text(question)
         redis_db.set(chat_id, question)
+        bot.send_message(chat_id=chat_id, text=question, reply_markup=reply_markup)
+    else:
+        question = redis_db.get(chat_id)
+        if not question:
+            return
+        answer = get_answer(question)
+        clean_answer = answer.partition('.')[0].partition('(')[0]
+        if clean_answer == message_text:
+            bot.send_message(chat_id=chat_id,
+                             text='Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»”',
+                             reply_markup=reply_markup)
+        else:
+            bot.send_message(chat_id=chat_id, text='Неправильно… Попробуешь ещё раз?', reply_markup=reply_markup)
 
 
 def main():
@@ -39,7 +53,7 @@ def main():
 
     quiz_db = auth_redis(redis_address, redis_port, redis_password)
     quiz_db.flushall()
-    
+
     updater = Updater(tg_token)
 
     dp = updater.dispatcher
