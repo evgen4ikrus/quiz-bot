b31a272d42b86597de6a7368846b806f64958119
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2022-11-02 17:39:15 +0300

    add the option to surrender

--- quiz_helpers.py
+++ quiz_helpers.py
@@ -13,4 +13,5 @@ def get_answer(question):
     with open('quiz_bank.json', 'r', encoding='UTF-8') as file:
         quiz_bank = json.load(file)
     answer = quiz_bank.get(question)
-    return answer
+    simple_answer = answer.partition('.')[0].partition('(')[0]
+    return simple_answer
--- tg_bot.py
+++ tg_bot.py
@@ -42,8 +42,7 @@ def handle_solution_attempt(bot, update, redis_db):
     if not question:
         return NEW_QUESTION
     answer = get_answer(question)
-    clean_answer = answer.partition('.')[0].partition('(')[0]
-    if clean_answer == message_text:
+    if answer == message_text:
         update.message.reply_text('Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»',
                                   reply_markup=reply_markup)
         return NEW_QUESTION
@@ -52,11 +51,21 @@ def handle_solution_attempt(bot, update, redis_db):
         return SOLUTION_ATTEMPT
 
 
-def process_text(bot, update):
+def handle_other_text(bot, update):
     menu_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счет']]
     reply_markup = telegram.ReplyKeyboardMarkup(menu_keyboard)
-    update.message.reply_text('Нажми на одну из кнопок', reply_markup=reply_markup)
+    update.message.reply_text('Нажми на кнопку "Новый вопрос"', reply_markup=reply_markup)
     return NEW_QUESTION
+
+
+def handle_defeat(bot, update, redis_db):
+    chat_id = str(update.effective_user.id)
+    question = redis_db.get(chat_id)
+    if not question:
+        return NEW_QUESTION
+    answer = get_answer(question)
+    update.message.reply_text(f'Правильный ответ: {answer}')
+    handle_new_question_request(bot, update, redis_db=redis_db)
 
 
 def main():
@@ -77,14 +86,16 @@ def main():
     dp = updater.dispatcher
     handle_solution_attempt_with_args = partial(handle_solution_attempt, redis_db=quiz_db)
     handle_new_question_request_with_args = partial(handle_new_question_request, redis_db=quiz_db)
+    handle_defeat_with_args = partial(handle_defeat, redis_db=quiz_db)
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
             NEW_QUESTION: [RegexHandler('Новый вопрос', handle_new_question_request_with_args)],
-            SOLUTION_ATTEMPT: [MessageHandler(Filters.text, handle_solution_attempt_with_args)],
+            SOLUTION_ATTEMPT: [RegexHandler('Сдаться', handle_defeat_with_args),
+                               MessageHandler(Filters.text, handle_solution_attempt_with_args)],
         },
-        fallbacks=[MessageHandler(Filters.text, process_text)]
+        fallbacks=[MessageHandler(Filters.text, handle_other_text)]
     )
     dp.add_handler(conv_handler)
 
